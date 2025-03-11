a502855fdec87766ed748ca674a6d9eaa0ffb621
Nitesh Nepali <pingnitesh07@gmail.com>
2025-03-11 13:39:11 +0545

    list tuple

--- tut1.py
+++ tut1.py
@@ -1,10 +1,18 @@
-a = "10"
-print("Before Typecasting")
-print(a)
-print(type(a))
+### List 
+al = [0,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+count = 0 
+for i in al :
+    if i == 0 :
+     count = 0 
+    else :
+       count = count + 1
+       print(i,end=",")
+
+print(count)
+
+print(al[26])
 
 
-print("After Typecasting")
-b = bool(a)
-print(b)
-print(type(b))
+t = (1,2,"nitesh")
+print(t)
+print(type(t))
