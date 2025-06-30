72c5df85c2c8068bf2dd9a16bb55544120df6b15
Nitesh Nepali <pingnitesh07@gmail.com>
2025-06-30 13:24:14 +0545

    session 7 completed

---
+++ session7.py
@@ -0,0 +1,31 @@
+# a,b= [int(x) for x in input("Enter a two number: ").split(",")] # list comprehensive and unpacking of list
+# print("The sume is", a+b)
+
+# Eval fucnction 
+# a = eval(input("Enter a anythin: "))
+# print(type(a))
+
+
+# Command line Argument 
+
+# NOTE : IN COMMAND LINE ARGUMENT ALWAYS USE DOUBLE QUOTATION FOR STRING
+
+# from sys import argv
+
+# print("The number of command line argument", len(argv))
+# print("The command line arguments are ", argv)
+# print("The number of command line argument one by one")
+
+# for x in argv:
+#     print(x)
+
+
+
+## python  output statement
+# print("Nitesh"+"Nepali")
+# print("Nitesh","Nepali")
+
+# a,b,c,d = 10,20,30,40
+# print("The values are",a,b,c,d,sep='==')
+
+
