3702eaecb48d90ea2747d33c4ddfb2217b45290c
Nitesh Nepali <pingnitesh07@gmail.com>
2025-06-30 15:38:13 +0545

    session 8 completed

---
+++ session8.py
@@ -0,0 +1,33 @@
+# FLOW CONTROL IN PYTHON 
+
+# name = input("Enter a name: ")
+# if name == "hhhh" :
+#     print("Name is  on the list")
+# else:
+#     print("Name is  not in the list")
+
+# num1 = eval(input("Enter the first number "))
+# num2 = eval(input("Enter the second number "))
+
+# if num1 > num2:
+#     print(f"{num1} is greater than {num2}")
+
+# elif num2> num1:
+#     print(f"{num2}  is greater than {num1}")
+
+# WAP to find smallest of given 2 numbers
+# WAP to find smallest of given 3 numbers
+# WAP to find given number is odd or even
+
+# Switch statement 
+
+# sub = input("Enter  a fav sub of Engineering :")
+
+# # match sub:
+# #     case "DSA":
+# #         print("")
+# #     case "MAth":
+# #         print("")
+# #     case _:
+# #         print("no subject")
+
