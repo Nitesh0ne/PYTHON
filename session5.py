f0602269c0ab58185b9672711a181b5390665230
Nitesh Nepali <pingnitesh07@gmail.com>
2025-07-08 23:24:28 +0545

    session 5 completed: operator

---
+++ session5.py
@@ -0,0 +1,85 @@
+# # # Escape Sequence 
+# # a = " python's \r baba funfdfasdfsdf"
+# # print(a)
+
+# # arithmetic opertator
+# #----------Arithmetic Operator in pytho ---------
+
+# a = 10 
+# b = 3
+# add = a + b 
+# print("Addition        : ",add)
+
+# sub = a -b 
+# print("subtraction     : ",sub)
+
+# mul = a*b
+# print("Multiplication  : ",mul)
+
+# div = a / b
+# print("Divsion         : ",div)
+
+# floor_div = a // b
+# print("Floor Division  : ",floor_div)
+
+# power = a**2
+# print("power           : ",power)
+
+# remainder = a % b 
+# print("Remainder       :", remainder)
+
+
+# # string concatenation string 
+# str1 = "First  String "
+# str2 = "Second String"
+
+# print("Concatenation String: ",str1+str2 )
+# print("String Repeatition  : ",str1*4)
+
+
+# # comparison or Relational 
+# #  Operator
+
+
+# # number comparison
+# # string Comparison
+# # boolean Comparison
+# print(10>2)
+# print("A">"a")
+
+# print(ord("a")) # print the ascii value 
+# print(chr(97)) # number to character 
+
+# # chaining of realational operator
+
+# a = 10<20<30<100
+# print(a)
+
+
+
+# 3. Equality Operatoi (== , !=)
+
+# print(10==20)
+# print(10!=20)
+# print(10==10)
+# print(10!=10)
+
+# print("A"=="A")
+
+# 4. Logical Operator ( and , or , not )
+
+# 4.1  Boolean Type 
+
+
+# 0 = false 
+# 1 and non-zero, non-empty  all are true 
+# empty object like list , set, tuple are false
+# result = 10 and 20
+# print(result)
+
+
+
+# example = 0 and 7
+# print(example)
+
+# 5. Bitwise operator 
