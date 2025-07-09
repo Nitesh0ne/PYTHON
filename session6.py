a27b2675a66b805eb331afa3864af717b145d5bb
Nitesh Nepali <pingnitesh07@gmail.com>
2025-07-09 22:38:40 +0545

    Session 6: ternary operator, special , identity , membership operator , math module complete

---
+++ session6.py
@@ -0,0 +1,26 @@
+#
+
+# special Operator
+
+# 1.1 identity operator : compare the address of the  variable
+
+# a = 10 
+# b = 10 
+# a = 20 
+
+# print(a is b )
+# print(a is not b )
+
+
+# list1 = [10,20,30]
+# list2 = [10,20,30]
+
+# print(list1 is list2)
+ 
+
+
+
+# Math Module 
+
+import 
+print(dir())
