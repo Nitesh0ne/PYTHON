5cd89aadc2efa3f5e222bfec08d208cab0343c42
Nitesh Nepali <pingnitesh07@gmail.com>
2025-07-11 19:23:34 +0545

    Session 9 : for loop , while loop , transfer statement completed

--- session9.py
+++ session9.py
@@ -4,7 +4,21 @@
 # for x in list:
 #     sum 
 
+# count = 0 
+# for x in "abcdefghijklmnopqrstuvwxyz":
+#     count = count + 1
+#     print(f"{count} {x}")
+# sum = 0
+# for x in range(1,20,2):
+#       sum = sum + x
+
+# print(sum)
+
+
+
 # 2. While Loop
+
+
 
 # name = input("Enter a name")
 # while name != "test":
@@ -16,18 +30,25 @@
 # while True:
 #     print("infinite")
 
+# i = 0
+# while True:
+#     i = i +1
+#     print(i)
+
+# Nested For loop 
+
 # for i in range(2):
 #     for j in range(2):
 #         print(i,j)
 
-s = "hello"
-a = s
-b = a 
-print(s)
-print(a)
-print(b)
-
-del s
+# s = "hello"
+# a = s
+# b = a 
 # print(s)
-print(a)
-print(b)
+# print(a)
+# print(b)
+
+# del s
+# # print(s)
+# print(a)
+# print(b)
