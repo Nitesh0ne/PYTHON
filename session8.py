fc7b9fe60da5436a88c5b2fce45be13cc9e3bca9
Nitesh Nepali <pingnitesh07@gmail.com>
2025-07-11 17:40:04 +0545

    Session 8 : Flow control, conditional , switch statement completed

--- session8.py
+++ session8.py
@@ -1,6 +1,9 @@
 # FLOW CONTROL IN PYTHON 
+# 1. Conditional Statment
+# 1.1 if Statement
 
-# name = input("Enter a name: ")
+
+# name = input("Enter a name: "
 # if name == "hhhh" :
 #     print("Name is  on the list")
 # else:
@@ -31,3 +34,36 @@
 # #     case _:
 # #         print("no subject")
 
+
+# switch statement 
+
+# subject= input("Enter your favour subject : ")
+
+# match subject:
+#     case "Network":
+#         print(f"Your favourite subject is {subject}")
+#     case "Programming":
+#         print(f"Your favourite subject is {subject}")
+#     case "Linux":
+#         print(f"Your favourite subject is {subject}")
+#     case "CEH" :
+#         print(f"Your favourite subject is {subject}")
+#     case _:
+#         print("You subject is not in the list ")
+
+# using Switch case 
+words_upto_19 = ["zero", "one", "two", "three", "Four", "five", "Six", "Seven", "eight", "nine", "ten", "eleven", "twleve", "thirteen", "Fourteen", "Fifteen", "sixteen", "Seventeen", "eighteen", "Nineteen"]
+
+words_for_tens = ["","", "twenty", "thirty", "fourtey", "fiftey", "Sixtey", "Sventy", "eighty"]
+
+
+n = int(input("Enter any number : "))
+
+if n == 0:
+    output= "zero"
+elif n<=19:
+    output= words_upto_19[n]
+elif n<=99:
+    output = words_for_tens[n//10]+" "+words_upto_19[n%10]
+
+print(output)
