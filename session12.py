40b161fa6dbbba13c77651cab1b2fa1b0f789859
Nitesh Nepali <pingnitesh07@gmail.com>
2025-07-29 21:28:15 +0545

    session 12 : tuple completed

--- session12.py
+++ session12.py
@@ -1,29 +1,21 @@
-# tuple
+# TUPLE 
+# Read only list and  tuple is same as list and tuple is  immutable 
 
-t1 = 10,20,30,40,
-print(type(t1))
+t1 = 10,20,30,10
 print(t1)
-t2 = () 
+print(type(t1))
+
+t2= ()
+print(t2)
 print(type(t2))
-t3 =(1,) # single value  tuple comma is must
+
+
+
+l1 = [1,2,3]
+t3 = tuple(l1)
+print(t3)
 print(type(t3))
 
 
-list= [1,2,3,4,6]
-t5 = tuple(list)
-print(type(t5))
 
 
-#Accessing  the tuple element 
-
-t6 = (0,1,2,3,4,5,6,7,8,9)
-
-print(t6[:])
-print(t6[0:])
-print(t6[:-1])
-print(t6[0:-1:2])
-
-
-# Some Important Function of tuple
-
-print(len(t6))
