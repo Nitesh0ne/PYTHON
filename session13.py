8a3bf3c65405b69686161fdc7f9dd60560f9559f
Nitesh Nepali <pingnitesh07@gmail.com>
2025-08-03 21:42:30 +0545

    session 13 : Set and dictionary completed

--- session13.py
+++ session13.py
@@ -1,11 +1,23 @@
-# dictionary 
+# # Set in python 
+# s = {10,20,30,40}
+# # s.pop()
+# print(s)
 
-d1= {
-   1:"a",
-   2: "b",
-   3:"c",
-}
-print(d1)
 
-d2={}
-print(d1[])
+
+# converting list into  the  dictionary 
+
+rec = {}
+
+n = int (input("Enter the number of students : "))
+i = 1
+
+while i <=n:
+    name = input ("Enter the name of the student: ")
+    marks = input ("Enter the marks of the student: ")
+    rec[name]=marksi = i+1
+
+print("The records of the student are :")
+
+for k,v in rec.items():
+    print(k,v)
